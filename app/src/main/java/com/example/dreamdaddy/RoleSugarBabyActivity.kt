9061e97e1148d8aae597e4169d52c6bb13c35e1b
Alvaro de Francisco <alfrasan@gmail.com>
2019-11-05 17:49:38 +0100

    Alvaro - Clases Activities
    Creadas las Activities asignadas

---
+++ app/src/main/java/com/example/dreamdaddy/RoleSugarBabyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dreamdaddy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RoleSugarBabyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_sugar_daddy_role)
+    }
+}
