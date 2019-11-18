8035ac9004f3c017174e85c4e04a5b4f6318edab
SaraPdrui <SaraPdrui@outlook.com>
2019-11-18 20:17:42 +0100

    Sara -  Cambio en las clases de Kotlin

---
+++ app/src/main/java/com/example/dreamdaddy/classes/Person.kt
@@ -0,0 +1,12 @@
+package com.example.dreamdaddy.classes
+
+import java.util.*
+
+open class Person {
+    var nickname: String = ""
+    var password: String = ""
+    var linkImage: String = ""
+    var birthDate: Date = Date()
+    var email: String = ""
+    var telephone: String = ""
+}
---
+++ app/src/main/java/com/example/dreamdaddy/classes/SuggarBaby.kt
@@ -0,0 +1,3 @@
+package com.example.dreamdaddy.classes
+
+class SuggarBaby : Person()
