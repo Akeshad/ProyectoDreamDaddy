6777dc38db91f582cbbe53da1925604bd0150a8b
SaraPdrui <SaraPdrui@outlook.com>
2019-11-20 13:54:46 +0100

    Alvaro y Sara - DatePicker Dialog añadido
    Hemos añadido un DatePicker en la clase RegisterActivity

--- app/src/main/java/com/example/dreamdaddy/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/dreamdaddy/activities/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dreamdaddy.activities
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,26 +9,41 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.dreamdaddy.R
 import com.example.dreamdaddy.classes.SugarBaby
 import com.example.dreamdaddy.classes.SugarDaddy
+import kotlinx.android.synthetic.main.layout_register.*
 import java.sql.Date
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_register)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        editTextBirthDateRegister.setOnClickListener{
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDayOfMonth ->
+                editTextBirthDateRegister.setText( "" + mDayOfMonth + "/" + mMonth + "/" + mYear)
+            }, year, month , day )
+
+            dpd.show()
+        }
+
     }
 
     fun subscribeNewUser(view: View) {
-
-        val sugarDaddy : SugarDaddy = intent.getSerializableExtra("sugardaddy") as SugarDaddy
-       // val sugarBaby : SugarBaby? = intent.getSerializableExtra("sugarbaby") as SugarBaby
-
-
+        var sugarDaddy : SugarDaddy = intent.getSerializableExtra("sugardaddy") as SugarDaddy
         if(sugarDaddy != null) {
             sugarDaddy.nickname = findViewById<EditText>(R.id.editTextUsernameRegister).text.toString()
             sugarDaddy.email = findViewById<EditText>(R.id.editTextEmailRegister).text.toString()
             sugarDaddy.password = findViewById<EditText>(R.id.editTextPasswordRegister).text.toString()
-            sugarDaddy.birthDate = findViewById<EditText>(R.id.editTextBirthDateRegister).text as Date
+            sugarDaddy.birthDate = Date.valueOf(findViewById<EditText>(R.id.editTextBirthDateRegister).text.toString())
             val intent = Intent(this, GridActivity::class.java)
             intent.putExtra("sugardaddy", sugarDaddy)
             startActivity(intent)
@@ -40,4 +56,6 @@ class RegisterActivity : AppCompatActivity() {
             sugarBaby.birthDate = findViewById<EditText>(R.id.editTextBirthDateRegister).text as Date
         }*/
     }
+
+
 }
