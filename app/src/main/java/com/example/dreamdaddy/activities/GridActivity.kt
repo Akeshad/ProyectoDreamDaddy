f7a85e47a47d34072fe7fc7c949dd5cc12ba06bd
SaraPdrui <SaraPdrui@outlook.com>
2019-11-20 12:49:41 +0100

    Sara - Recoger datos en Register Activity

--- app/src/main/java/com/example/dreamdaddy/activities/GridActivity.kt
+++ app/src/main/java/com/example/dreamdaddy/activities/GridActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dreamdaddy.R
 
-class GridDaddysActivity : AppCompatActivity() {
+class GridActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/dreamdaddy/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/dreamdaddy/activities/RegisterActivity.kt
@@ -1,9 +1,14 @@
 package com.example.dreamdaddy.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dreamdaddy.R
+import com.example.dreamdaddy.classes.SugarBaby
+import com.example.dreamdaddy.classes.SugarDaddy
+import java.sql.Date
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,7 +19,25 @@ class RegisterActivity : AppCompatActivity() {
 
     fun subscribeNewUser(view: View) {
 
-        val sugarDaddy = intent.getSerializableExtra("sugardaddy")
+        val sugarDaddy : SugarDaddy = intent.getSerializableExtra("sugardaddy") as SugarDaddy
+       // val sugarBaby : SugarBaby? = intent.getSerializableExtra("sugarbaby") as SugarBaby
 
+
+        if(sugarDaddy != null) {
+            sugarDaddy.nickname = findViewById<EditText>(R.id.editTextUsernameRegister).text.toString()
+            sugarDaddy.email = findViewById<EditText>(R.id.editTextEmailRegister).text.toString()
+            sugarDaddy.password = findViewById<EditText>(R.id.editTextPasswordRegister).text.toString()
+            sugarDaddy.birthDate = findViewById<EditText>(R.id.editTextBirthDateRegister).text as Date
+            val intent = Intent(this, GridActivity::class.java)
+            intent.putExtra("sugardaddy", sugarDaddy)
+            startActivity(intent)
+        }
+
+     /*  if(sugarBaby != null){
+            sugarBaby.nickname = findViewById<EditText>(R.id.editTextUsernameRegister).text.toString()
+            sugarBaby.email = findViewById<EditText>(R.id.editTextEmailRegister).text.toString()
+            sugarBaby.password = findViewById<EditText>(R.id.editTextPasswordRegister).text.toString()
+            sugarBaby.birthDate = findViewById<EditText>(R.id.editTextBirthDateRegister).text as Date
+        }*/
     }
 }
