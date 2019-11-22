915616028d9a941ccacd37beeb61ef799cbff56b
SaraPdrui <SaraPdrui@outlook.com>
2019-11-22 11:06:13 +0100

    Todos - Menú bar + Cambios Estéticos
    Hemos hecho una prueba de implementación de un Menú de Opciones en GridActivity.
    Hemos quitado el ActionBar en general de la app.
    Hemos añadido layouts, strings, colores y estilos.

--- app/src/main/java/com/example/dreamdaddy/activities/GridActivity.kt
+++ app/src/main/java/com/example/dreamdaddy/activities/GridActivity.kt
@@ -1,7 +1,11 @@
 package com.example.dreamdaddy.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dreamdaddy.R
 import com.example.dreamdaddy.classes.SugarDaddy
@@ -15,6 +19,7 @@ class GridActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_grid_profiles)
 
+
         gridView = findViewById(R.id.gridLayoutGridProfiles)
 
         val daddies = ArrayList<SugarDaddy>()
@@ -24,5 +29,36 @@ class GridActivity : AppCompatActivity() {
 
         gridAdapter = GridAdapter(this, daddies)
         gridView.adapter = gridAdapter
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.layout.layout_option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_search -> {
+            msgShow("Search")
+            true
+        }
+        R.id.action_profile -> {
+            msgShow("Profile")
+            true
+        }
+        R.id.action_setting -> {
+            msgShow("Setting")
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun msgShow(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
